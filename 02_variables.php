9cca530f68de2a7a15075fdba5fe0d16be671b5c
Ozgur Ozhan <ozgur2096@gmail.com>
2024-01-15 13:08:15 +0100

    variables1

--- 02_variables.php
+++ 02_variables.php
@@ -1,14 +1,40 @@
 <?php
 
-// What is a variable
-
 // Variable types
+/*
+String
+Integer
+Float / Double
+Boolean
+Null
+Array
+Object
+Resource
+*/
 
 // Declare variables
+$name = 'Ozy';
+$age = 33;
+$isMale = true;
+$anotherBoolean  = false;
+$height = 1.81;
+$salary = null;
 
 // Print the variables. Explain what is concatenation
+echo 'Name: ' . $name . '<br>';
+echo 'Age: ' . $age . '<br>';
+echo 'isMale: ' . $isMale . '<br>';
+echo 'anotherBoolean: ' . $anotherBoolean . '<br>';
+echo 'Height: ' . $height . '<br>';
+echo 'Salary: ' . $salary . '<br>' . '<br>';
 
 // Print types of the variables
+echo gettype($name) . '<br>';
+echo gettype($age) . '<br>';
+echo gettype($isMale) . '<br>';
+echo gettype($anotherBoolean) . '<br>';
+echo gettype($height) . '<br>';
+echo gettype($salary) . '<br>' . '<br>';
 
 // Print the whole variable
 
