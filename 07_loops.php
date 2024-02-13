791e6633345713c9252da4749bba8359009edbeb
Ozgur Ozhan <ozgur2096@gmail.com>
2024-02-13 16:43:03 +0100

    loops1

--- 07_loops.php
+++ 07_loops.php
@@ -1,12 +1,27 @@
 <?php
 
 // while
-
-// Loop with $counter
+$counter = 0;
+while ($counter < 10) {
+  echo $counter . '</br>';
+  // if ($counter === 5) break;
+  $counter += 1;
+};
+echo '</br>';
 
 // do - while
+$counter2 = 0;
+do {
+  echo $counter2 . '</br>';
+  $counter2++;
+} while ($counter2 < 10);
+echo '</br>';
 
 // for
+for ($i = 0; $i < 10; $i++) {
+  echo $i . '</br>';
+}
+echo '</br>';
 
 // foreach
 
