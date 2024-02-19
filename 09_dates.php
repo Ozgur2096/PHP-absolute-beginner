083f430ba939695cd13f936cfa2a6356d2098808
Ozgur Ozhan <ozgur2096@gmail.com>
2024-02-19 12:13:54 +0100

    date

--- 09_dates.php
+++ 09_dates.php
@@ -1,13 +1,31 @@
 <?php
 
 // Print current date
+echo date('d-m-Y h:i:s');
+echo '<br>';
 
 // Print yesterday
+echo date('d-m-Y h:i:s', time() - 60 * 60 * 24);
+echo '<br>';
 
 // Different format: https://www.php.net/manual/en/function.date.php
+echo date('F j Y h:i:s');
+echo '<br>';
 
 // Print current timestamp
+echo time();
+echo '<br>';
 
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
+$parsedDate = date_parse('2024-2-15 09:00:00');
+// echo '<pre>';
+// var_dump($parsedDate);
+// echo '</pre>';
 
 // Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
+$dateString = 'February 4 63 13:00:00';
+
+$parsedDate2 = date_parse_from_format('F j y h:i:s', $dateString);
+echo '<pre>';
+var_dump($parsedDate2);
+echo '</pre>';
